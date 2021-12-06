6ddffbae7a0a257a39f60b33f37298bdbcc57f18
laci79 <68596968+laci79@users.noreply.github.com>
2021-12-06 02:53:36 +0100

    c-f update

--- thermo/celsius-fahrenheit-conv.js
+++ thermo/celsius-fahrenheit-conv.js
@@ -2,16 +2,17 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const inputfield1 = document.getElementById("celsius");
     const inputfield2 = document.getElementById("fahrenheit");
-    const myform = document.getElementById("my-form");
 
     document.getElementById("celsius").oninput = () => {
 
         inputfield2.value = +(inputfield1.value * 9 / 5 + 32).toFixed(2);
 
         if (inputfield1.value <= 150 && inputfield1.value >= -50) {
-            $('#div').animate({height: parseInt(inputfield1.value) + 66})
+            $('#div').animate({height: parseInt(inputfield1.value) + 66});
+        } else if (inputfield1.value > 150) {
+            $('#div').animate({height: 216});
         } else {
-            $('#div').animate({height: 66})
+            $('#div').animate({height: 16});
         }
     }
 
@@ -21,8 +22,10 @@ document.addEventListener('DOMContentLoaded', () => {
 
         if (inputfield1.value <= 150 && inputfield1.value >= -50) {
             $('#div').animate({height: parseInt(inputfield1.value) + 66})
+        } else if (inputfield1.value > 150) {
+            $('#div').animate({height: 216});
         } else {
-            $('#div').animate({height: 66})
+            $('#div').animate({height: 16})
         }
     }
 })
